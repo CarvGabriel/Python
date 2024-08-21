484ea7eed46659718ca73230be89b54b71bbc516
Gabriel Carvalho <142913657+CarvGabriel@users.noreply.github.com>
2024-08-21 18:05:41 -0300

    second commit

--- APICotacao.py
+++ APICotacao.py
@@ -7,8 +7,31 @@ cotacaoDolar = cotacao["USDBRL"]["bid"]
 cotacaoEuro = cotacao["EURBRL"]["bid"]
 cotacaoBTC = cotacao["BTCBRL"]["bid"]
 
-valor = float(input("Digite um valor em reais: "))
+while True:
 
-print(f'O valor em dolar é de {valor / float(cotacaoDolar)}')
-print(f'O valor em euro é de {valor / float(cotacaoEuro)}')
-print(f'O valor em bitcoin é de {valor / float(cotacaoBTC)}')
+    print("Escolha um conversor:\n")
+    print("1- Real para dolar")
+    print("2- Real para euro")
+    print("3- Real para bitcoin")
+    print("0- Encerrar")
+
+    escolha = input("\nDigite o número escolhido: ")
+
+    if escolha in ("123"):
+        valor = float(input("\nDigite um valor em reais: "))
+
+        if escolha == "1":
+            print(f'\nO valor em dolar é de {valor / float(cotacaoDolar)}')
+        elif escolha == "2":
+            print(f'\nO valor em euro é de {valor / float(cotacaoEuro)}')
+        else:
+            print(f'\nO valor em bitcoin é de {valor / float(cotacaoBTC)}')
+
+    else:
+        if escolha =="0":
+            break
+        else:
+            print("\nEntrada inválida")
+    
+    x = input("\nPressione ENTER para continuar")
+    print("\n" * 30)
